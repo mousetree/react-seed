b192400b7163f1e8c824a626d80ae36f3d1950db
Adam Mausenbaum <adam@admm.io>
2016-12-06 17:12:06 +0200

    test travis env vars

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const travisVars = [
+	'TRAVIS_BUILD_NUMBER',
+	'TRAVIS_COMMIT',
+	'TRAVIS_JOB_NUMBER'
+];
+
 const About = (props) => (
 	<div>
 		<h1>About</h1>
+		{travisVars.map((v) => {
+			return (
+				<p>{v}: {process.env[v] || 'N/A'}</p>
+			)
+		})}
 	</div>
 );
 
