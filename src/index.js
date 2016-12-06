9fe14192cf276b5bbb8d82843d69273a0b12c9ab
Adam Mausenbaum <adam@admm.io>
2016-12-06 13:33:40 +0200

    add google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import ga from 'react-ga';
 
 import Products from './pages/Products';
 import About from './pages/About';
@@ -9,8 +10,14 @@ import Home from './pages/Home';
 import App from './App';
 import './index.css';
 
+if (process.env.NODE_ENV === 'production' && process.env.GA_ID) {
+  ga.initialize(process.env.GA_ID);
+}
+
+const logPageView = () => ga.pageview(window.location.pathname);
+
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/products" component={Products} />
