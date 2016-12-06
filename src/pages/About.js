b10d92c215c5e6b862ed3f24e7a10bc729231949
Adam Mausenbaum <adam@admm.io>
2016-12-06 18:04:29 +0200

    abandon travis/heroku env vars

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,8 @@
 import React from 'react';
 
-const travisVars = [
-	'REACT_APP_TRAVIS_BUILD_NUMBER',
-	'REACT_APP_TRAVIS_COMMIT',
-	'REACT_APP_TRAVIS_JOB_NUMBER',
-	'REACT_APP_BUILD_DATE'
-];
-
 const About = (props) => (
 	<div>
 		<h1>About</h1>
-		{travisVars.map((v) => {
-			return (
-				<p key={v}>{v.substr(10)}: {process.env[v] || 'N/A'}</p>
-			)
-		})}
 	</div>
 );
 
