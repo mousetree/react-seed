78b58da61dd1b6e02fab533667cee9e58021bac2
Adam Mausenbaum <adam@admm.io>
2016-12-06 13:23:28 +0200

    remove material-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import NavBar from './components/Navigation/NavBar';
-import NavDrawer from './components/Navigation/NavDrawer';
-
-injectTapEventPlugin();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    };
-  }
-
-  handleToggle = () => this.setState({open: !this.state.open});
 
   render() {
     const styles = {
@@ -24,15 +11,12 @@ class App extends Component {
     };
 
     return (
-      <MuiThemeProvider>
         <div>
-          <NavBar onMenuToggle={this.handleToggle}/>
-          <NavDrawer open={this.state.open} onToggle={this.handleToggle}/>
+          <NavBar/>
           <div className="page" style={styles.page}>
             {this.props.children}
           </div>
         </div>
-      </MuiThemeProvider>
     );
   }
 }
--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
+import {Link} from 'react-router';
+
+const styles = {
+	container: {
+		borderBottom: '1px solid whitesmoke',
+		padding: 24,
+		display: 'flex',
+		alignItems: 'center'
+	},
+	title: {
+		fontSize: 24,
+		color: 'black',
+		textDecoration: 'none'
+	},
+	menu: {},
+	menuItem: {
+		color: 'grey',
+		textDecoration: 'none',
+		marginLeft: 32
+	},
+	activeMenuItem: {
+		color: 'black'
+	}
+};
 
 const NavBar = (props) => (
-	<AppBar
-		title="React Seed"
-		onLeftIconButtonTouchTap={() => props.onMenuToggle()}
-	/>
+	<div style={styles.container}>
+		<Link style={styles.title} to="/">
+			Seed
+		</Link>
+		<div style={styles.menu}>
+			<Link to="/about" style={styles.menuItem} activeStyle={styles.activeMenuItem}>About</Link>
+			<Link to="/products" style={styles.menuItem} activeStyle={styles.activeMenuItem}>Products</Link>
+		</div>
+	</div>
 );
 
 export default NavBar;
